6cee5d06b9416d455bc25d31c3ad3d54aee0a4d9
Michael Davis <michael.davis@liatrio.com>
2025-06-02 08:50:19 -0700

    fix: create directory in workflow

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { ReshiPic } from './components/ReshiPics';
 export type { ReshiPic as ReshiPicType } from './types';
+// Re-export the plugin for Backstage to find it
+export { reshiPicHomepage, ReshiPicHomepage } from './plugin';
